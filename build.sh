7f299b4c7f51844bdb179e81c54270781ac54a00
Lucas Gallindo Martins Soares <lgms@cin.ufpe.br>
2022-11-29 00:17:28 -0300

    Script for unconditional build

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-sudo apt-get install libopencv-dev libopencv-dnn-dev
+sudo apt-get install libopencv-d7ev libopencv-dnn-dev
+sed -i 's/GPU=1/GPU=0/' Makefile
+rm -r build
+mkdir build
 cd build
-cmake .. -DENABLE_CUDA=OFF & cmake --build .
+cmake .. -DENABLE_CUDA=OFF & cmake --build . & rm ../darknet & cp darknet ../darknet
 cd ..
-rm darknet
-cp build/darknet darknet
+sed -i 's/GPU=0/GPU=1/' Makefile
+rm -r build_gpu
+mkdir build_gpu
+cd build_gpu
+cmake .. & cmake --build . & rm ../darknet_gpu & cp darknet_gpu ../darknet_gpu
+cd ..
